8968250b5f7191d3b09146243a87c268e11eeb3f
matuzbatista <matuzalem_pereira@hotmail.com>
2023-11-07 19:27:23 -0300

    Criando API de Partidas

---
+++ ApiPartidaApplication.java
@@ -0,0 +1,13 @@
+package com.meli.apipartida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPartidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPartidaApplication.class, args);
+    }
+
+}
